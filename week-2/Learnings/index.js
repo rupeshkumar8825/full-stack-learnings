69e652cf92baca8b73483d6768c716de44d612c3
rupeshkumar8825 <therupeshkumar8825@gmail.com>
2023-12-17 18:47:35 +0530

    difference between the settimeout and setinternval

---
+++ week-2/Learnings/index.js
@@ -0,0 +1,7 @@
+// in this file we will be writing the learnings of the week - 2 assignments and classes 
+
+
+// DIFFERENCE BETWEEN THE SETTIMEOUT AND SETINTERVAL 
+// the basic difference between them is that the settimeout is used to run a piece of code after some delay.
+// but in case of the setinterval we can run the same piece of code after each interval 
+// we can also clear the setinterval and the settimeout functions to stop executing the same stuff for this purpose 
